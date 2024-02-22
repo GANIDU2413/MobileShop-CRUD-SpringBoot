74d74142386e3563815e114f8c4ae5ed31b02e52
GANIDU2413 <ganidusahan@gmail.com>
2024-02-22 16:15:40 +0530

    Initial commit

---
+++ src/main/java/com/workshop/mobileshop/controller/MobileController.java
@@ -0,0 +1,8 @@
+package com.workshop.mobileshop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MobileController {
+}
---
+++ src/main/java/com/workshop/mobileshop/module/Mobile.java
@@ -0,0 +1,13 @@
+package com.workshop.mobileshop.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+public class Mobile {
+}
